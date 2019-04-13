4c7f5a59e34748ff660e2ce32e8d0d76f25023af
Shai-Miller <49593356+Shai-Miller@users.noreply.github.com>
2019-04-13 20:16:48 +0300

    Add files via upload

---
+++ University social network/Client/include/outputThread.h
@@ -0,0 +1,21 @@
+
+#ifndef CLIENT_OUTPUTTHREAD_H
+#define CLIENT_OUTPUTTHREAD_H
+
+#include "connectionHandler.h"
+
+class outputThread {
+private:
+    int * kill;
+    ConnectionHandler & _connectionHandler;
+public:
+
+    outputThread(ConnectionHandler & connectionHandler , int *Terminate);
+    void run();
+    bool DecodeMsg(char* result);
+    short bytesToShort(char *bytesArr);
+
+};
+
+
+#endif //CLIENT_OUTPUTTHREAD_H
